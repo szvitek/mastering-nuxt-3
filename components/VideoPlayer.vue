174307cc937b4ca7342ccd7365a9516827dcb5e1
szvitek <gy.szvitek@gmail.com>
2024-01-20 23:07:02 +0000

    feat(Navigation): navigating with NuxtLink, useHead for dynamic page titles

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 const props = defineProps<{
-  videoId: string;
+  videoId: number;
 }>();
 </script>
 
--- pages/course.vue
+++ pages/course.vue
@@ -13,10 +13,29 @@
 
     <div class="flex flex-row justify-center flex-grow">
       <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[15em] flex flex-col"
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[15em] max-w-[20em] flex flex-col"
       >
         <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
+        <div
+          class="spece-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1"
+            :to="lesson.path"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath,
+            }"
+          >
+            <span class="text-gray-500">{{ index + 1 }}</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
       </div>
 
       <div class="prose p-12 bg-white rounded-md w-[41em] max-w-[41em]">
@@ -25,3 +44,7 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const { chapters } = useCourse();
+</script>
