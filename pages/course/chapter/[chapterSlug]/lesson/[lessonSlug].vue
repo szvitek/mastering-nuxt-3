460159fa5cdc28d1787fe2b91ed8bfab60784814
szvitek <gy.szvitek@gmail.com>
2024-01-20 14:54:02 +0000

    feat(LessonPage): Loading in Course Data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -2,7 +2,24 @@
   <div>
     <h2>Lesson</h2>
     <p>This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+    <p>{{ chapter.title }}</p>
+    <p>{{ lesson.title }}</p>
   </div>
 </template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
