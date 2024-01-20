c2a7a3ad5c9038b89603f817fb2754ba8a6aceba
szvitek <gy.szvitek@gmail.com>
2024-01-20 14:34:45 +0000

    feat(Routes): dynamic and nested routes

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Lesson</h2>
+    <p>This is a lesson</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
