fb12513c3557736bc746d881c85fb1ab72db2ffc
szvitek <gy.szvitek@gmail.com>
2024-01-20 21:34:16 +0000

    feat(VideoPlayer): add video player component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  videoId: string;
+}>();
+</script>
+
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  />
+</template>
+
+<style scoped></style>
