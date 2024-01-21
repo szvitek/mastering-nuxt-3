3e2b53ce41c7d12998bc36a9fbd270c4557aabf9
szvitek <gy.szvitek@gmail.com>
2024-01-21 16:18:19 +0000

    feat(404): adding a 404 error page

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLayout>
+    <div class="prose">
+      <template v-if="error.statusCode === 404">
+        <h1>404</h1>
+        <p>I guess that page doesn't exist.</p>
+      </template>
+
+      <template v-else>
+        <h1>Dang</h1>
+        <p>
+          <strong>{{ error.message }}</strong>
+        </p>
+        <p>It looks like something broke.</p>
+        <p>Sorry about that.</p>
+      </template>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+</script>
