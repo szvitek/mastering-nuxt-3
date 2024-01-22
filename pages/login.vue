bf1a9e48ea8ed41590cc8aeb9bf0584acf1986c0
szvitek <gy.szvitek@gmail.com>
2024-01-22 21:42:37 +0000

    feat(Login): login with github

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="prose w-full max-w-2xl h-9">
     <h1>Log in to {{ title }}</h1>
-    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+    <button
+      class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      @click="login"
+    >
       Log in with Github
     </button>
   </div>
@@ -9,4 +12,19 @@
 
 <script setup lang="ts">
 const { title } = useCourse();
+
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm',
+    },
+  });
+
+  if (error) {
+    console.error(error);
+  }
+};
 </script>
