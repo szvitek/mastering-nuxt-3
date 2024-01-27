0cf8b3ca4f56c2466f2b0d2e17c14f32f32b2d26
szvitek <gy.szvitek@gmail.com>
2024-01-27 19:57:26 +0000

    feat(Prisma): getting data from prisma

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,32 +1,24 @@
-import course from '~/server/courseData';
+import { PrismaClient } from '@prisma/client';
 
-course as Course;
+const prisma = new PrismaClient();
 
-export default defineEventHandler((event): LessonWithPath => {
+export default defineEventHandler(async (event) => {
   const chapterSlug = getRouterParam(event, 'chapterSlug');
   const lessonSlug = getRouterParam(event, 'lessonSlug');
 
-  const chapter: Maybe<Chapter> = course.chapters.find(
-    (chapter) => chapter.slug === chapterSlug
-  );
-  if (!chapter) {
-    throw createError({
-      statusCode: 404,
-      message: 'Chapter not found',
-    });
-  }
-  const lesson: Maybe<Lesson> = chapter.lessons.find(
-    (lesson) => lesson.slug === lessonSlug
-  );
-  if (!lesson) {
-    throw createError({
-      statusCode: 404,
-      message: 'Lesson not found',
-    });
-  }
-
-  return {
-    ...lesson,
-    path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
-  };
+  return prisma.lesson.findFirst({
+    where: {
+      Chapter: {
+        slug: chapterSlug,
+      },
+    },
+    include: {
+      Chapter: {
+        select: {
+          slug: true,
+          title: true,
+        },
+      },
+    },
+  });
 });
