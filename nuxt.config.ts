86393a58ccbaef456fd5a86b66705307709bd4e5
szvitek <gy.szvitek@gmail.com>
2024-02-14 20:00:45 +0000

    feat(PreRender): Static page generation and pre render the landing page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      routes: ['/landing']
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
