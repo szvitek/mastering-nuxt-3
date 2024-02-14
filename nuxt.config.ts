c8127c69b6e19342f8f9cee11a0b67e9c307e5af
szvitek <gy.szvitek@gmail.com>
2024-02-14 21:02:50 +0000

    feat(Stripe): setup stripe env vars + runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import vsharp from 'vite-plugin-vsharp';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    stripeSecret: '',
+    public: {
+      stripeKey: '',
+    },
+  },
   nitro: {
     prerender: {
       routes: ['/landing'],
